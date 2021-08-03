8fd42adab4708afdb8ce28b19ba6c0df913073e8
hesnablgl <hesna.bilgili@pru.edu.tr>
2021-08-03 17:40:39 +0300

    GÜNCELLEME

--- WebApplication3/Models/CategoryModel.cs
+++ WebApplication3/Models/CategoryModel.cs
@@ -10,10 +10,10 @@ namespace WebApplication3.Models
     public class CategoryModel
     {
 
-        [DisplayName("Title"), Required(ErrorMessage ="{0} cannot be empty!"), MaxLength(10,ErrorMessage = "A maximum of {10} characters must be entered!")]
+        [DisplayName("Title"), Required(ErrorMessage ="{0} cannot be empty!"), MaxLength(10,ErrorMessage = "A maximum of {1} characters must be entered in {0}!")]
         public string Title { get; set; }
 
-        [DisplayName("Description"), Required(ErrorMessage = "{0} cannot be empty!"), MinLength(5, ErrorMessage = "At least {1} characters must be entered!"]
+        [DisplayName("Description"), Required(ErrorMessage = "{0} cannot be empty!"), MinLength(5, ErrorMessage = "At least {1} characters must be entered in {0}!")]
         public string Description { get; set; }
 
     }
